bb47407a051a0db5320669ccab66cec521e39a42
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-18 22:13:11 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240118/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed;
+    cout.precision(3);
+    cout << a << endl << b << endl<< c;
+    return 0;
+}
