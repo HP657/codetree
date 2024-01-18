f82af2868f3da6652eed4a772b41612a57945416
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-18 22:28:54 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240118/전화번호 바꾸기/change-phone-number.cpp
+++ 240118/전화번호 바꾸기/change-phone-number.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a, b, c;
+    int b, c;
+    string a;
     char _;
     cin >> a >> _ >> b >> _ >> c;
     cout << a << _ << c << _ << b;
