649dabd12b18128c0b8720dcedfef408f26f2698
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-19 20:47:07 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a == 100)
+    {
+        cout << "pass";
+    }
+    else
+    {
+        cout << "failure";
+    }
+    return 0;
+}
