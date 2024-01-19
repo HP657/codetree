f27985babf8292158911bb3262e33164e33fd763
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-19 20:23:47 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240119/합과 평균/sum-and-avg.cpp
+++ 240119/합과 평균/sum-and-avg.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << a + b << " " << double(double(a + b) / 2);
+    cout << fixed;
+    cout.precision(1);
+    cout << a + b << " " << double(a + b) / 2;
     return 0;
 }
