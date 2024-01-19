3324eb4d52f0ac403f2af320c1d84974fa573654
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-19 20:41:37 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a >= 80)
+    {
+        cout << "pass";
+    }
+    else
+    {
+        cout << 80 - a << " more score";
+    }
+    return 0;
+}
