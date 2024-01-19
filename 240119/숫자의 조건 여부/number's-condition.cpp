21c542ed049a34db3f6f408a45278957708e2633
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-19 20:42:35 +0900

    Title: 숫자의 조건 여부, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/숫자의 조건 여부/number's-condition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a >= 113)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
