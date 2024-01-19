cf23e7b7df216029634d8ffb82923a1502d54355
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-19 20:56:58 +0900

    Title: 출석 부르기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240119/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a == 1)
+    {
+        cout << "John";
+    }
+    else if (a == 2)
+    {
+        cout << "Tom";
+    }
+    else
+    {
+        cout << "Paul";
+    }
+    return 0;
+}
