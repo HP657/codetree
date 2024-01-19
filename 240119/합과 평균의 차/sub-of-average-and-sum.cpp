6581915aa5d0f7b22af7e6b6710dcfaf484a2161
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-19 20:26:42 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl << (a + b + c) / 3 << endl << (a + b + c) - ((a + b + c) / 3);
+    return 0;
+}
