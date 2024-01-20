784ab1af38de9e35e01cceeed2ea925e49cc59dd
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-20 16:02:06 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240120/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char a;
+    cin >> a;
+    if (a == 'S')
+    {
+        cout << "Superior";
+    }
+    else if (a == 'A')
+    {
+        cout << "Excellent";
+    }
+    else if (a == 'B')
+    {
+        cout << "Good";
+    }
+    else if (a == 'C')
+    {
+        cout << "Usually";
+    }
+    else (a == 'D')
+    {
+        cout >> "Effort";
+    }
+    return 0;
+}
