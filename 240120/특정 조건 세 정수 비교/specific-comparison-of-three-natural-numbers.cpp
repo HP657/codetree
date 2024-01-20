82a659e91102c7007398466e7b561dfcb9b44cc2
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-20 16:39:54 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240120/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a < b && a < c)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    cout << ' ';
+    if (a == b == c)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
