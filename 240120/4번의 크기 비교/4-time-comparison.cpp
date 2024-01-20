73d7f9e72541f3277432bb1412d40a4084950bfa
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-20 16:27:59 +0900

    Title: 4번의 크기 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, d, e;
+    cin >> a;
+    cin >> b >> c >> d >> e;
+    cout << (a > b) << endl;
+    cout << (a > c) << endl;
+    cout << (a > d) << endl;
+    cout << (a > e) << endl;
+    return 0;
+}
