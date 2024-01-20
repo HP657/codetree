3dab85c72b6a110a64ce02c7f4c2e0fca574327d
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-20 16:51:03 +0900

    Title: 세 수의 중간값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, m;
+    cin >> a >> b >> c;
+    if (a < b && c > b)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
