33179edc713cf2024a8d9fbf5be3248e2bab678d
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-20 16:05:11 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240120/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >. a;
+    if (a >= 3000)
+    {
+        cout << "book";
+    }
+    if (a >= 1000)
+    {
+        cout << "mask";
+    }
+    if (a >= 500)
+    {
+        cout << "pen";
+    }
+    else
+    {
+        cout << "no";
+    }
+    return 0;
+}
