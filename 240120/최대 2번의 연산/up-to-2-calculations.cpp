94fb9a1b1fab3bf5895cf7f414dc1164de9aa218
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-20 16:09:35 +0900

    Title: 최대 2번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/최대 2번의 연산/up-to-2-calculations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a % 2 == 0)
+    {
+        a = a / 2;
+    }
+    if (a % 2 == 1)
+    {
+        a = (a + 1) / 2;
+    }
+    cout << a;
+    return 0;
+}
