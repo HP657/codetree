c58b86f5bfee0f08485d376e1b00e11252a506cc
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-20 17:04:16 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240120/장학금/scholarship.cpp
+++ 240120/장학금/scholarship.cpp
@@ -3,20 +3,13 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    if (a >= 90)
+    if (a >= 90 && b >= 95)
     {
-        if (b >= 95)
-        {
-            cout << 10;
-        }
-        else if (b >= 90)
-        {
-            cout << 5;
-        }
-        else 
-        {
-            cout << 0;
-        }
+        cout << 10;
+    }
+    if (a >= 90 && b >= 90)
+    {
+        cout << 5;
     }
     else
     {
