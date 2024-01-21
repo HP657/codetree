fae4c038737331c717756da591becdc8cb1aeb4a
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-21 18:20:54 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240121/두 사람/two-person.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    char c, d;
+    cin >> a >> c;
+    cin >> b >> d;
+    if ((a >= 19 || c >= 19) && (c == 'M' || d == 'M'))
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
