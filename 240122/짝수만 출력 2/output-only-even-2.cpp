7735459b4fc9f3ee67b1e30982c64d964a75a72e
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-22 18:46:09 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    while (a >= b)
+    {
+        if (a % 2 == 0)
+        {
+            cout << a << ' ';
+        }
+        a--;
+    }
+    return 0;
+}
