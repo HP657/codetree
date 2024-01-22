6996436d6da1105b6195bd00f28afb1524f70f13
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-22 19:22:22 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/369 게임/369-game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 || i % 6 == 0 || i % 9 == 0)
+        {
+            cout << 0 << ' ';
+        }
+        else
+        {
+            cout << i;
+        }
+    }
+    return 0;
+}
