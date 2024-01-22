924287197dd37fa1d69fd9a6017fbda09201d5b7
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-22 19:54:13 +0900

    Title: a부터 b까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240122/a부터 b까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, sum = 0;
+    cin >> a >> b;
+    if (int i = a; i <= b; i++)
+    {   
+        sum += i;
+    }
+    cout << sum
+    return 0;
+}
