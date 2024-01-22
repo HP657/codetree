1399a154aa1ab4f41516a837ee15b784b9a6b17b
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-22 19:10:06 +0900

    Title: 2 또는 3의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240122/2 또는 3의 배수/multiple-of-2-or-3.cpp
+++ 240122/2 또는 3의 배수/multiple-of-2-or-3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int a;
     cin >> a;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= a; i++)
     {
         if (a % 2 == 0 || a % 3 == 0)
         {
