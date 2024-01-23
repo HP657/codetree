5a25c3b10157d1a93d8498e17a64e317792fe049
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-23 21:06:42 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    while (true)
+    {
+        cin >> a;
+        if (a == 0)
+        {
+            break;
+        }
+        cout << a << endl;
+    }
+    return 0;
+}
