a25a8e237667dbe84846e561a813e30805fba3e3
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-23 20:54:38 +0900

    Title: 1부터 곱셈하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, prod = 1;
+    cin >> n;
+    for (int i = 1; i <= 10; i++)
+    {
+        prod *= i;
+        if (prod >= n)
+        {
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}
