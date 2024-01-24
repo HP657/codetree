e6cacf0833e77eec3ef364605a549a85bc7c6131
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-24 18:14:15 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    bool a = false;
+    for (int i = 2; i <= n; i++)
+    {
+        if (n / i != 1)
+        {
+            a = true;
+        }
+    }
+    if (a)
+    {
+        cout << "C";
+    }
+    else
+    {
+        cout << "N";
+    }
+    return 0;
+}
