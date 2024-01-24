e28497ef9cfc8f762bae6fb245db17e9bebcba23
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-24 18:52:19 +0900

    Title: 건너뛰고 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << '*';
+        }
+        cout << endl << endl;
+    }
+    for (int i = n - 1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << '*';
+        }
+        cout << endl << endl;
+    }
+    return 0;
+}
