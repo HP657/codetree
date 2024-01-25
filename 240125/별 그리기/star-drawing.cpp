0552a274e4e38baa8c4e250b20f5bf518d34c7d7
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-25 19:08:09 +0900

    Title: 별 그리기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/별 그리기/star-drawing.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - i - 1; j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int j = ((n * 2) - 1); j >= ((n * 2) - 1) - (i * 2); j--)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    for (int i = n - 1; i > 0; i--)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << ' ';
+        }
+        for (int j = n; j >= n - (i * 2); j--)
+        {
+            cout <<'*';
+        }
+        cout << endl;
+    }
+    return 0;
+}
