e82b94dc5103a4354f2bcf0847f4ca2e5ff5c73d
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-25 18:11:50 +0900

    Title: 별표 출력하기 9, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/별표 출력하기 9/print-star-9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1, j = 1; i <= n; i++, j += 2)
+    {
+        for (int k = 2 * (n - i - 1); k >= 0; k--)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < j; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
