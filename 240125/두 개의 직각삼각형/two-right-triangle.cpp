459d240fba7abf0fb3c6384874ba4eedf0d2bcd2
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-25 18:17:14 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/두 개의 직각삼각형/two-right-triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = n; i >= 0; i--)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            cout << '*';
+        }
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = i; j > 0; j--)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    return 0;
+}
