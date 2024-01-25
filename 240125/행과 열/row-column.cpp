534fadd6fbc66631757affa384870e5def68eeee
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-25 21:09:42 +0900

    Title: 행과 열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/행과 열/row-column.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x, y;
+    cin >> x >> y;
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 1; j <= y; j++)
+        {
+            cout << i * j << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
