e5529991bf8237ced7951387036a16b528c36252
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-25 19:39:13 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n - i; j > 0; j--)
+        {
+            cout << ' ';
+        }
+        for (int j = n - i; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = i; j <= n - 1; j++)
+        {
+            cout << ' ';
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
