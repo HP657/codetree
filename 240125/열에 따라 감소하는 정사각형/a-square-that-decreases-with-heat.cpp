5dfccc8e125f1980afbaa3133308f21c20bb5342
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-25 21:07:29 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
+++ 240125/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i = 0; i < n; i--)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = n; j > 0; j++)
+        for (int j = n; j > 0; j--)
         {
             cout << j << ' ';
         }
