609b11e4372a0be53e7128825d72982b09d9fe97
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-25 19:44:07 +0900

    Title: 별표 출력하기 8, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/별표 출력하기 8/print-star-8.cpp
+++ 240125/별표 출력하기 8/print-star-8.cpp
@@ -13,7 +13,7 @@ int main() {
         {
             for (int j = 0; j < i; j++)
             {
-                cout << '* ';
+                cout << "* ";
             }
         }
         cout << endl;
