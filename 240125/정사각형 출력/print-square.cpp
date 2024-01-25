c78646a15d4f53c01ea095b9861f7797e36a9ff0
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-25 21:26:14 +0900

    Title: 정사각형 출력, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/정사각형 출력/print-square.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int cnt = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << cnt << ' ';
+            cnt++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
