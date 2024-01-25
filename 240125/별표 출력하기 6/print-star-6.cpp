2bd1950272ac50202c487b30acfa5546d752caa2
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-25 18:51:31 +0900

    Title: 별표 출력하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/별표 출력하기 6/print-star-6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 0; j < (n * 2) - 1 - (i * 2); j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = n - 2; i >= 0; i --)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 0; j < (n * 2) - 1 - (i * 2); j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
