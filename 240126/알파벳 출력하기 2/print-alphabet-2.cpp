b8802a5c8f5b508e41cd6dafe65adf3b37fcaaeb
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-26 17:06:48 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240126/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, m = 65;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j > n - i; j--)
+        {
+            cout << "  ";
+        }
+        for (int j = n; j > i; j--, m++)
+        {
+            cout << char(m) << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
