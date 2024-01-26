3c8adf2251f7b7d732bc51eea021b82dd31823ee
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-26 15:45:17 +0900

    Title: 상하로 반복하며 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240126/상하로 반복하며 출력/print-in-up-down-reverse.cpp
+++ 240126/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -3,27 +3,18 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int a[n][n];
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            if (i % 2 == 1)
+            if (j % 2 == 1)
             {
-                a[j][i] = j;
+                cout << i;
             }
             else
             {
-                a[j][i] = n - j + 1;
+                cout << n - i + 1;
             }
-            
-        }
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            cout << a[i][j];
         }
         cout << endl;
     }
