466d8fcdb9b280e46f31f5472442bcba4418a285
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-26 15:55:42 +0900

    Title: 좌우 지그재그 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240126/좌우 지그재그 출력/print-lr-zigzag.cpp
+++ 240126/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -4,11 +4,11 @@ int main() {
     int n, cnt = 1;
     cin >> n;
     int a[n][n];
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n; j++)
         {
-            if (j % 2 == 1)
+            if (i % 2 == 0)
             {
                 a[i][j] = cnt;
                 cnt++;
@@ -21,11 +21,11 @@ int main() {
 
         }
     }
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n; j++)
         {
-            cout << a[i][j];
+            cout << a[i][j] << ' ';
         }
         cout << endl;
     }
