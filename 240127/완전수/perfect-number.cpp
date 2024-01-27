1266d0f88b51914f2211ba635a31c7d711f82cf7
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-27 19:57:07 +0900

    Title: 완전수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/완전수/perfect-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, m, cnt = 0, sum = 0;
+    cin >> n >> m;
+    for (int i = n; i <= m; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                sum += j;
+            }
+        }
+        if (i == sum)
+        {
+            cnt += 1;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
