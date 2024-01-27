695a3e880c969e03115e1eed42ac34b2df82dd4d
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-27 20:06:36 +0900

    Title: 약수의 개수가 3개인 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240127/약수의 개수가 3개인 수/square-of-prime-number.cpp
+++ 240127/약수의 개수가 3개인 수/square-of-prime-number.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n, m, cnt = 0, cntt = 0;
+    int n, m, cnt, cntt = 0;
     cin >> n >> m;
     for (int i = n; i <= m; i++)
     {
+        cnt = 0;
         for (int j = 1; j < i; j++)
         {
             if (i % j == 0)
@@ -16,7 +17,6 @@ int main() {
         {
             cntt += 1;
         }
-        cnt = 0;
     }
     cout << cntt;
     return 0;
