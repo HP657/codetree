7fb009618044b24d4864ae9913e952ed979b4aee
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-27 20:36:07 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240127/특정 숫자 도달하기/reaching-specific-number.cpp
+++ 240127/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -2,20 +2,19 @@
 using namespace std;
 int main() {
     int a[10], j;
+    int sum = 0, cnt = 0;
     for (int i = 0; i < 10; i++)
     {
         cin >> a[i];
         if (a[i] >= 250)
         {
-            j = i - 1;
             break;
         }
-    }
-    int sum = 0, cnt = 0;
-    for (int i = 0; i <= j; i++)
-    {
-        sum += a[i];
-        cnt++;
+        else
+        {
+            sum += a[i];
+            cnt++;
+        }
     }
     double avg = (double)sum / cnt;
     cout << fixed;
