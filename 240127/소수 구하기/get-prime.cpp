39789df1fdc05d9d9ba754a2a228a04725afd3ec
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-27 20:17:52 +0900

    Title: 소수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240127/소수 구하기/get-prime.cpp
+++ 240127/소수 구하기/get-prime.cpp
@@ -9,7 +9,7 @@ int main() {
     }
     for (int i = 2; i <= n; i++)
     {
-        for (int j = 2; j < i; j++)
+        for (int j = 3; j < i; j++)
         {
             if (i % j != 0)
             {
