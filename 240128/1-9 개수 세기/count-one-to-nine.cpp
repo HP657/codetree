24a38f11d9b2148c8908d639c362fd9c0066c769
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-28 12:29:04 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240128/1-9 개수 세기/count-one-to-nine.cpp
+++ 240128/1-9 개수 세기/count-one-to-nine.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a[10] = {0}, n, b;
+    int n, b;
     cin >> n;
+    int a[n] = {0};
     for (int i = 0; i < n; i++)
     {
         cin >> b;
