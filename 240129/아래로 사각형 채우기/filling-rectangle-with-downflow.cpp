490d0d76892e1bdc803bf270deef503837b65f4d
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-29 21:38:20 +0900

    Title: 아래로 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, q = 1;
+    cin >> n;
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            a[j][i] = q;
+            q++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << a[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
