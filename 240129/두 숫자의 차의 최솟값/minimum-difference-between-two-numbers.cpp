31ca5c411565bdcc9e13f6019019f259772544e8
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-29 20:40:04 +0900

    Title: 두 숫자의 차의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240129/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.cpp
+++ 240129/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.cpp
@@ -10,7 +10,7 @@ int main() {
     }
     for (int i = 0; i < n; i++)
     {
-        if (a[i + 1] - a[i] < min)
+        if (a[i + 1] - a[i] <= min)
         {
             min = a[i + 1] - a[i];
         }
