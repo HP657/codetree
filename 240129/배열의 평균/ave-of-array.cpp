317f3a83dc82599a50f51deae991c030a2fb4284
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-29 20:57:37 +0900

    Title: 배열의 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240129/배열의 평균/ave-of-array.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << fixed;
+    cout.precision(1);
+    int a[2][4];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> a[i][j];   
+        }
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            sum += a[i][j];
+        }
+        cout << sum / 4.0 << ' ';
+    }
+    cout << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < 2; j++)
+        {
+            sum += a[j][i];
+        }
+        cout << sum / 2.0 << ' ';
+    }
+    cout << endl;
+    double sum = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            sum += a[j][i];
+        }
+        cout << sum / 8.0 << ' ';
+    }
+    return 0;
+}
