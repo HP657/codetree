c592040a6506c4099feaa72f13ad8a9fc4c51260
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-29 19:19:38 +0900

    Title: n개의 숫자 중 최대 2개, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
+++ 240129/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
@@ -5,7 +5,7 @@ int main() {
     int n, max = INT_MIN, m;
     cin >> n;
     int a[n];
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> a[i];
         if (max <= a[i])
