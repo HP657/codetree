718d029b5165913185d37519ca6df4038ebfa9cd
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-29 21:11:44 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a[4][4];
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i >= j)
+            {
+                sum += a[i][j];
+            }
+        }
+    }
+    cout << sum;
+    return 0;
+}
