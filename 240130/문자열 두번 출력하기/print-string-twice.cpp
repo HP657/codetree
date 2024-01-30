2a09aa906f66bd0efb65e3226208881c964fdcdd
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-30 18:58:17 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    cin >> str;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << str << endl;
+    }
+    return 0;
+}
