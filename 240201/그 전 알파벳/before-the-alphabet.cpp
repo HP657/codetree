1727719baaf5cf58f8290dd747a55ce963bcd163
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-01 07:18:51 +0900

    Title: 그 전 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240201/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char a;
+    cin >> a;
+    int b = int(a) - 1;
+    if (b == 97)
+    {
+        b = 122;
+    }
+    cout << char(b);
+    return 0;
+}
