9fcbf933a55d422b7fc0bf27b93b47b911c38523
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-31 19:33:10 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240131/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a;
+    cin >> a;
+    for (int i = 0; i < a.length(); i++)
+    {
+        cout << a[i] << endl;
+    }
+    return 0;
+}
