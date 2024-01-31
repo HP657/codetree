2b779e2b99aa09d6e591967700cd0c5a7411c7c4
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-01 07:11:30 +0900

    Title: 아스키코드표 맞추기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> a;
+        cout << char(a) << ' ';
+    }
+    return 0;
+}
