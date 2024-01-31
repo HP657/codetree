ca77db54d836d13d6365fe0a3530dc972e2d7137
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-01 07:24:02 +0900

    Title: 대문자로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/대문자로 출력하기/print-in-capital.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a;
+    cin >> a;
+    for (int i = 0; i < a.length(); i++)
+    {
+        if (int(a[i]) >= 65 && int(a[i]) <= 90)
+        {
+            cout << a[i];
+        }
+        else if (int(a[i]) >= 97 && int(a[i]) <= 122)
+        {
+            cout <<char(int(a[i] - 32));
+        }
+    }
+    return 0;
+}
