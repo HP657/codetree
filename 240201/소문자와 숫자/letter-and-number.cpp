2f268d6f3e4c3b37b335698a5425296d9cf74ed2
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-01 07:29:04 +0900

    Title: 소문자와 숫자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240201/소문자와 숫자/letter-and-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a;
+    cin >> a;
+    for (int i = 0; i < a.length(); i++)
+    {
+        if ((int(a[i]) >= 48 && int(a[i]) <= 57) || (int(a[i]) >= 97 && int(a[i]) <= 122))
+        {
+            cout << a[i];
+        }
+        else if ((int(a[i]) >= 65) && (int(a[i]) <= 90))
+        {
+            cout << char(int(a[i] + 32));
+        }
+    }
+    bool num = (48 57)
+    return 0;
+}
