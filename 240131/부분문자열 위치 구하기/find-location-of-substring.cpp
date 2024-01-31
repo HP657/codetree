46e6002cf28fdf9fa2bdcbaa76446c24c400409d
이효준 <102369002+HP657@users.noreply.github.com>
2024-01-31 20:24:17 +0900

    Title: 부분문자열 위치 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240131/부분문자열 위치 구하기/find-location-of-substring.cpp
+++ 240131/부분문자열 위치 구하기/find-location-of-substring.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     string a;
     cin >> a;
     string b;
     cin >> b;
-    int k = -1;
-    int cnt = 0;
-    bool p = false;
-    for (int i = 0; i < a.length(); i++)
-    {
-        for (int j = 0; j < b.length(); j++)
-        {
-            if (a[i + j] == b[j])
-            {
-                cnt++;
-            }
-            else
-            {
-                cnt = 0;
-            }
-            if (cnt == b.length())
-            {
-                k = i;
+
+    int k = -1; // Initialize the result index to -1
+
+    for (int i = 0; i <= a.length() - b.length(); i++) {
+        bool match = true; // Assume a match at index i
+
+        for (int j = 0; j < b.length(); j++) {
+            if (a[i + j] != b[j]) {
+                match = false; // Set match to false if characters don't match
                 break;
             }
         }
+
+        if (match) {
+            k = i; // Update the result index if a match is found
+            break; // Break out of the loop since we found the first match
+        }
     }
-    cout << k;
+
+    cout << k; // Output the result index
     return 0;
 }
