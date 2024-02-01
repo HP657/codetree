be77ccc1a8b02b5bc7693c6ad861a4834fd5e9eb
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-01 18:11:53 +0900

    Title: 두 수의 합과 1, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240201/두 수의 합과 1/two-nums-sum-and-1.cpp
+++ 240201/두 수의 합과 1/two-nums-sum-and-1.cpp
@@ -4,11 +4,12 @@ int main() {
     int a, b;
     cin >> a >> b;
     int sum = a + b;
-    sum = to_string(sum);
+    string c;
+    c = to_string(sum);
     int cnt = 0;
-    for (int i = 0;i < sum.length(); i++)
+    for (int i = 0;i < c.length(); i++)
     {
-        if(sum[i] == 1)
+        if(c[i] == 1)
         {
             cnt++;
         }
