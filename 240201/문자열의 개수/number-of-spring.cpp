02c7f42c55db09dbff08518c0f48bdaa8047658b
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-01 18:37:23 +0900

    Title: 문자열의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240201/문자열의 개수/number-of-spring.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a[201];
+    int cnt = 0;
+    while(true)
+    {
+        cin >> a[cnt];
+        if (a[cnt] == "0")
+        {
+            break;
+        }
+        cnt++
+    }
+    cout << cnt << endl;
+    for (int i = 0; i < cnt; i += 2)
+    {
+        cout << str[i] << endl;
+    }
+    return 0;
+}
