cf87f6d4ef10b60d52a3e9ef19744aa06e61bb60
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-01 18:49:07 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/미는 횟수/number-of-pushes.cpp
+++ 240201/미는 횟수/number-of-pushes.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 int main() {
-    string a, b;
-    cin >> a >> b;
-    int cnt = 0;
-    while (true)
-    {
-        if (a == b)
-        {
-            break;
-        }
-        if (cnt == b.length())
-        {
-            cnt = -1;
-            break;
-        }
-        a = a.substr(1, a.length()) + a.substr(0, 1);
-        cnt++;
+    string A, B;
+    cin >> A >> B;
+
+    // A의 길이가 B의 길이보다 짧으면 B를 만들 수 없음
+    if (A.length() < B.length()) {
+        cout << -1 << endl;
+        return 0;
     }
-    cout << cnt;
+
+    int min_shift = -1; // 가능한 최솟값을 저장할 변수, 초기값은 -1로 설정
+
+    // 문자열 A를 우측으로 회전시키면서 B와 비교
+    for (int i = 0; i < A.length(); ++i) {
+        string rotated_A = A.substr(A.length() - i) + A.substr(0, A.length() - i);
+        if (rotated_A == B) {
+            min_shift = i; // 회전한 횟수를 최솟값으로 저장
+            break;
+        }
+    }
+
+    cout << min_shift << endl;
 
     return 0;
 }
