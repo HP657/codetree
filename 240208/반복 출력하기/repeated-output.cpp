53b7ea9b8d13974b4206395dc8c11f0a9e8b5984
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-08 19:40:46 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void aa(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        cout << "12345^&*()_";
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    aa(n);
+    return 0;
+}
