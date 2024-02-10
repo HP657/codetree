659fa4499e5df36db0f92791e252700c961e64b4
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-10 23:10:35 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240210/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 240210/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int t(int a, int b) {
     int sum = 0;
     for (int i = a; i <= b; i++) {
+        if (b == 1) {
+            break;
+        }
         bool f = true;
         for (int j = 2; j < i; j++) {
             if (i % j == 0) {
