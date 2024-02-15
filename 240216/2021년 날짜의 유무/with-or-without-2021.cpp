d03fe8828c03e55e803c36bcd68e763c8bf39b87
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-16 08:46:59 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240216/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+bool dd (int d, int ddd) {
+    if (ddd < d) {
+        return false;
+    }
+    return true;
+}
+
+bool mm(int m, int d) {
+    int ddd;
+    if (m % 2 == 1) {
+        if (m <= 7) {
+            ddd = 31;
+        }
+        else {
+            ddd = 30;
+        }
+    }
+    else{
+        if (m <= 7) {
+            ddd = 30;
+        }
+        else if (m == 2) {
+            ddd = 28;
+        }
+        else {
+            ddd = 31;
+        }
+    }
+    
+    return dd(d, ddd);
+}
+
+int main() {
+    int m, d;
+    cin >> m >> d;
+    if (mm(m, d)) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+}
