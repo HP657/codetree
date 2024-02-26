bcda7dfc3787c1de9631e765705499445c33fc79
이효준 <102369002+HP657@users.noreply.github.com>
2024-02-26 10:54:17 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void aa(int n) {
+    if (n == 0) {
+        return;
+    }
+    aa(n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << '*';
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    aa(n);
+    return 0;
+}
